1b9ae177d58f52dc9d9fc8b04b1bb5be7b96fa15
Daniluvatar <daniluvatar@gmail.com>
2018-03-02 13:11:23 -0500

    Create TwitterAccountInfo.py

---
+++ TwitterAccountInfo.py
@@ -0,0 +1,14 @@
+import tweepy
+
+
+class Profile:
+
+    def __init__(self, api):
+        self.api = api
+
+    def following(self):
+        following_dic = {}
+        for friend in tweepy.Cursor(self.api.friends).items():
+            # following_list.append(friend._json)
+            following_dic[friend._json['screen_name']] = friend._json
+        return following_dic
